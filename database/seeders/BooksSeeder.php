e9ff4471c5b0a9d6e33b58ff273134b1866ccacb
Kiichi14 <100412917+Kiichi14@users.noreply.github.com>
2023-09-04 19:54:03 +0200

    first commit

---
+++ database/seeders/BooksSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class BooksSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        for($i = 0; $i < 5; $i++) {
+            DB::table('books')->insert([
+                'title' => $faker->unique()->word,
+                'resume' => $faker->unique()->paragraph,
+                'category_id' => $faker->numberBetween(1, 5),
+                'author_id' => $faker->numberBetween(1, 5),
+                'editor_id' => $faker->numberBetween(1, 5)
+            ]);
+        }
+    }
+}
