d0c3e8ba4bb4997a6635ce48b00b15986005ecc1
Kiichi14 <100412917+Kiichi14@users.noreply.github.com>
2023-09-05 09:10:25 +0200

    create relation in books model

--- app/Models/Books.php
+++ app/Models/Books.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Books extends Model
 {
     use HasFactory;
+
+    public function category(): HasOne
+    {
+        return $this->hasOne(Category::class, 'id', 'category_id');
+    }
+
+    public function editor(): HasOne
+    {
+        return $this->hasOne(Editor::class, 'id', 'editor_id');
+    }
+
+    public function author(): HasOne
+    {
+        return $this->hasOne(Author::class, 'id', 'author_id');
+    }
 }
