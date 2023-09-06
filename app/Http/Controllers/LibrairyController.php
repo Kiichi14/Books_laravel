3cd4ae1d3ec37ce3c7ebf1623bb9c5b30b565fc3
Kiichi14 <100412917+Kiichi14@users.noreply.github.com>
2023-09-06 09:30:46 +0200

    format json librairy

--- app/Http/Controllers/LibrairyController.php
+++ app/Http/Controllers/LibrairyController.php
@@ -30,12 +30,17 @@ class LibrairyController extends Controller
 
     public function show($id) {
 
-        $librairy = Librairy::with('user', 'editions.book.category', 'editions.book.author')->where('user_id', $id)->get();
+        $librairy = Librairy::with('user', 'editions.book.category', 'editions.book.author', 'editions.editions')->where('user_id', $id)->get();
+
+        $editions = $librairy->pluck('editions');
 
         return response()->json([
             'status_code' => 200,
             'status_message' => 'Détail de la bibliothéque',
-            'bibliothéque' => $librairy
+            'result' => [
+                'user' => $librairy->first()->user,
+                'bibliothéque' => $editions
+            ]
         ]);
 
     }
